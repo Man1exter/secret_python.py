ba7727969498b046a89a51e1a67897f1f877126d
Mariusz Perzynski <m.perzyk@interia.pl>
2021-04-22 13:23:41 +0200

    create menu

--- secrets.py
+++ secrets.py
@@ -1,1 +1,15 @@
-print("Hello World")
+
+def choice_way():
+    pass
+
+def menu():
+    print("..CHOICE YOUR WAY TO LEARN NEW METHOD IN PYTHON..")
+    print("[1]")
+    print("[2]")
+    print("[3]")
+    print("[4]")
+    print("[5]")
+
+def main():
+    menu()
+    choice_way()
